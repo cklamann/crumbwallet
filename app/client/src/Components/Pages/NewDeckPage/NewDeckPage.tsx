1d519792247a9b43a50e9bd654ef8108a89a9b65
conor <conorklamann@gmail.com>
2020-12-31 16:40:09 -0500

    improve error messages on deck page, separate chess and regular deck creation methods

--- app/client/src/Components/Pages/NewDeckPage/NewDeckPage.tsx
+++ app/client/src/Components/Pages/NewDeckPage/NewDeckPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useReducer } from 'react';
+import React, { useContext, useReducer, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -8,6 +8,8 @@ import { UserContext } from '../../App';
 import { useGoTo } from 'Hooks';
 import { FullWidthTextField } from 'Shared';
 import * as Chess from 'chess.js';
+import { Typography } from '@material-ui/core';
+import { SelectionState } from 'draft-js';
 
 interface ReducerState {
     categories: string[];
@@ -32,8 +34,18 @@ const NewDeckForm: React.FC<{}> = ({}) => {
         [addCard] = useAddCardMutation(),
         [generateImage] = useCreateChessDiagramPngUrlMutation(),
         [state, dispatch] = useReducer(reducer, INITIAL_STATE),
+        [fenError, setFenError] = useState<string>(),
+        [pgnError, setPgnError] = useState<string>(),
         userId = useContext(UserContext),
         goto = useGoTo(),
+        getSubmitDisabled = () => {
+            const { chess, name, pgn } = state;
+            if (!!name) {
+                if (!chess) return false;
+                if (pgn) return false;
+            }
+            return true;
+        },
         updateField = <T extends keyof ReducerState>(field: T) => (value: ReducerState[T]) =>
             dispatch({ type: 'update', payload: { [field]: value } }),
         composeBuildDiagramFn = (deckId: string) => (fen: string, side: 'w' | 'b', turn: number, nextMove: string) =>
@@ -48,23 +60,51 @@ const NewDeckForm: React.FC<{}> = ({}) => {
                         prompt: `${side === 'b' ? 'Black' : 'White'} to move`,
                     },
                 })
-            );
+            ),
+        createChessDeck = () => {
+            const { fen, pgn } = state;
+            try {
+                validateChessDeck(pgn, fen);
+            } catch (e) {
+                setChessErrors(e);
+            }
+            setPgnError(null);
+            setFenError(null);
 
-    //todo: clean this up
-    const createDeck = () =>
-        addDeck({
-            variables: { name: state.name, userId, private: state.private, type: state.chess ? 'chess' : null },
-        })
-            .then((res) => res.data.createDeck.id)
-            .then((deckId) => {
-                if (state.chess) {
-                    return Promise.all(buildDiagram(state.pgn, composeBuildDiagramFn(deckId), state.fen)).then(
-                        () => deckId
-                    );
-                } else return deckId;
+            addDeck({
+                variables: { name: state.name, userId, private: state.private, type: 'chess' },
             })
-            .then((deckId) => goto(`/decks/${deckId}/edit`))
-            .catch((err) => console.error(err));
+                .then(
+                    ({
+                        data: {
+                            createDeck: { id },
+                        },
+                    }) => {
+                        return Promise.all(buildDiagram(state.pgn, composeBuildDiagramFn(id), state.fen))
+                            .then(() => id)
+                            .catch((e) => console.log(`caught: ${e}`));
+                    }
+                )
+                .then((deckId) => goto(`/decks/${deckId}/edit`))
+                .catch((err) => console.error(err));
+        },
+        createDeck = () =>
+            addDeck({
+                variables: { name: state.name, userId, private: state.private, type: null },
+            })
+                .then(({ data: { createDeck: { id } } }) => goto(`/decks/${id}/edit`))
+                .catch((err) => console.error(err)),
+        setChessErrors = (e: ChessError) => {
+            switch (e.type) {
+                case INVALID_PGN:
+                    return setPgnError(e.message);
+                case INVALID_FEN:
+                    return setFenError(e.message);
+                default:
+                    console.error(e);
+                    return;
+            }
+        };
 
     return (
         <Grid container wrap="wrap" alignItems="center">
@@ -108,8 +148,10 @@ const NewDeckForm: React.FC<{}> = ({}) => {
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                                 updateField('fen')(e.currentTarget.value)
                             }
+                            error={!!fenError}
                             label="FEN"
                         />
+                        {fenError && <Typography color="error">{fenError}</Typography>}
                     </Grid>
                     <Grid item xs={10}>
                         <FullWidthTextField
@@ -117,16 +159,18 @@ const NewDeckForm: React.FC<{}> = ({}) => {
                             onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                                 updateField('pgn')(e.currentTarget.value)
                             }
+                            error={!!pgnError}
                             required={state.chess}
                             label="PGN"
                         />
+                        {pgnError && <Typography color="error">{pgnError}</Typography>}
                     </Grid>
                 </>
             )}
             <Grid item xs={2}>
                 <Button
-                    disabled={!state.name || (state.chess && !state.pgn)}
-                    onClick={() => createDeck()}
+                    disabled={getSubmitDisabled()}
+                    onClick={() => (state.chess ? createChessDeck() : createDeck())}
                     variant="contained"
                 >
                     Go
@@ -147,14 +191,27 @@ const reducer = (state: ReducerState, action: { type: string; payload: Partial<R
 
 export default NewDeckForm;
 
-const buildDiagram = (
-    pgn: string,
-    create: (fen: string, side: 'w' | 'b', turn: number, nextMove: string) => Promise<any>,
-    fen?: string
-) => {
-    
-    validatePgn(pgn, !!fen);
+const validateChessDeck = (pgn: string, fen?: string) => {
+    //@ts-ignore
+    const instance = Chess();
+    if (fen) {
+        const validatedFen = instance.validate_fen(fen);
+        if (!validatedFen.valid) {
+            throw invalidFen(validatedFen.error);
+        }
+    }
+    const moves = getMovesFromPgn(pgn);
 
+    moves.forEach((m, i) => {
+        if (instance.move(m) === null) {
+            throw invalidPgn(`${m} is not allowed!`);
+        }
+    });
+
+    return true;
+};
+
+const getMovesFromPgn = (pgn: string) => {
     const moves = pgn
         .replace(/\[.+\]/, '')
         .replace(/\{\.+\}/g, '')
@@ -164,8 +221,17 @@ const buildDiagram = (
         .map((mv) => mv.trim())
         .filter((mv) => /^\w/.test(mv));
 
-    if (!moves.length) throw invalidPgn('No moves detected in pgn!');
+    if (!moves.length) throw invalidPgn('No moves in pgn!');
 
+    return moves;
+};
+
+const buildDiagram = (
+    pgn: string,
+    create: (fen: string, side: 'w' | 'b', turn: number, nextMove: string) => Promise<any>,
+    fen?: string
+) => {
+    const moves = getMovesFromPgn(pgn);
     //remove score if exists
     moves[moves.length - 1].replace(/ \d.+/, '');
 
@@ -173,7 +239,7 @@ const buildDiagram = (
     const chess = Chess();
 
     if (fen) {
-        loadFen(chess, fen);
+        chess.load(fen);
     }
 
     const fens = [];
@@ -191,15 +257,13 @@ const buildDiagram = (
     return fens.map((f, i) => create(f, f.split(' ')[1], +f.split(' ')[5], moves[i]));
 };
 
-const validatePgn = (pgn: string, hasFen: boolean) => {
-    if (!pgn.trim().startsWith('1.') && !hasFen) {
-        throw invalidPgn('PGN with no FEN must start with move 1!');
-    }
-    return true;
-};
-
 const INVALID_PGN = 'INVALID_PGN',
     INVALID_FEN = 'INVALID_FEN';
+
+interface ChessError {
+    message: string;
+    type: string;
+}
 
 const invalidPgn = (message: string) => ({
     message,
@@ -210,11 +274,3 @@ const invalidFen = (message: string) => ({
     message,
     type: INVALID_FEN,
 });
-
-const loadFen = (chessInstance: any, fen: string) => {
-    const validated = chessInstance.validate_fen(fen);
-    if (!validated.valid) {
-        throw invalidFen(validated.error);
-    }
-    return chessInstance.load(fen);
-};
